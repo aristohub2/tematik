fc7721c6f6f925316faa29c91eaa53628804bc19
aristohub <54464081+aristohub@users.noreply.github.com>
2019-10-17 09:15:57 +0700

    Create tematik.sql

---
+++ tematik.sql
@@ -0,0 +1,327 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.16 : Database - tematik
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`tematik` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `tematik`;
+
+/*Table structure for table `tmtk_banner` */
+
+DROP TABLE IF EXISTS `tmtk_banner`;
+
+CREATE TABLE `tmtk_banner` (
+  `IdBanner` int(11) NOT NULL,
+  `BannerTitle` varchar(20) DEFAULT NULL,
+  `BannerDescription` varchar(50) DEFAULT NULL,
+  `BannerPath` varchar(200) DEFAULT NULL,
+  `dCreateOn` date DEFAULT NULL,
+  `sStatusDelete` varchar(10) DEFAULT NULL,
+  `dDeleteOn` date DEFAULT NULL,
+  PRIMARY KEY (`IdBanner`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `tmtk_banner` */
+
+/*Table structure for table `tmtk_bracelet` */
+
+DROP TABLE IF EXISTS `tmtk_bracelet`;
+
+CREATE TABLE `tmtk_bracelet` (
+  `IdBracelet` int(11) NOT NULL,
+  `BraceletName` varchar(50) DEFAULT NULL,
+  `BraceletDescription` varchar(1000) DEFAULT NULL,
+  `UploadFk` varchar(100) NOT NULL,
+  `dCreateOn` date DEFAULT NULL,
+  `sStatusDelete` varchar(10) DEFAULT NULL,
+  `dDeleteOn` date DEFAULT NULL,
+  PRIMARY KEY (`IdBracelet`,`UploadFk`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `tmtk_bracelet` */
+
+/*Table structure for table `tmtk_earrings` */
+
+DROP TABLE IF EXISTS `tmtk_earrings`;
+
+CREATE TABLE `tmtk_earrings` (
+  `IdEarrings` int(11) NOT NULL,
+  `EarringsName` varchar(50) DEFAULT NULL,
+  `EarringsDescription` varchar(100) DEFAULT NULL,
+  `UploadFk` varchar(50) DEFAULT NULL,
+  `dCreateOn` date DEFAULT NULL,
+  `sStatusDelete` varchar(10) DEFAULT NULL,
+  `dDeleteOn` date DEFAULT NULL,
+  PRIMARY KEY (`IdEarrings`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `tmtk_earrings` */
+
+/*Table structure for table `tmtk_necklaces` */
+
+DROP TABLE IF EXISTS `tmtk_necklaces`;
+
+CREATE TABLE `tmtk_necklaces` (
+  `Idnecklencess` int(11) NOT NULL,
+  `necklencessName` varchar(50) DEFAULT NULL,
+  `necklencessDescription` varchar(1000) DEFAULT NULL,
+  `UploadFk` varchar(100) DEFAULT NULL,
+  `dCreateOn` date DEFAULT NULL,
+  `sStatusDelete` varchar(10) DEFAULT NULL,
+  `dDeleteOn` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `tmtk_necklaces` */
+
+/*Table structure for table `tmtk_product_uploads` */
+
+DROP TABLE IF EXISTS `tmtk_product_uploads`;
+
+CREATE TABLE `tmtk_product_uploads` (
+  `IdUpload` varchar(100) DEFAULT NULL,
+  `ProductId` int(11) NOT NULL,
+  `ProductPath` varchar(100) DEFAULT NULL,
+  `dCreateOn` date DEFAULT NULL,
+  `sStatusDelete` varchar(10) DEFAULT NULL,
+  `dDeleteOn` date DEFAULT NULL,
+  PRIMARY KEY (`ProductId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `tmtk_product_uploads` */
+
+/* Procedure structure for procedure `sp_tmtk_banner` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `sp_tmtk_banner` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_tmtk_banner`( p_sMode CHAR(1),	p_IdBanner INT,
+	p_BannerTitle CHAR(20),
+	p_BannerDescription CHAR(50),
+	p_BannerPath CHAR(200)
+)
+BEGIN
+IF p_sMode = 'I' THEN 
+BEGIN
+DECLARE oId INT;
+	SET oId = (SELECT CASE WHEN COUNT(1) = 0 THEN 1 ELSE MAX(IdBanner) + 1 END FROM tmtk_banner);
+ INSERT INTO tmtk_banner ( 			IdBanner,
+			BannerTitle,
+			BannerDescription,
+			BannerPath,
+			dCreateOn ) 
+			VALUES ( 			oId,
+			p_BannerTitle,
+			p_BannerDescription,
+			p_BannerPath,
+			CURRENT_TIMESTAMP); 
+END;
+ELSEIF p_sMode = 'U' THEN 
+BEGIN
+ UPDATE sp_tmtk_banner SET 
+nUnitId_fk = p_nUnitId_fk,
+nSeqNo = p_nSeqNo,
+sUUID = MD5(UUID()),
+sLastEditBy = p_sUserInput, dLastEditOn = CURRENT_TIMESTAMP 
+WHERE nUserId_fk = p_nUserId_fk  ;
+END;
+ELSEIF p_sMode = 'D' THEN 
+BEGIN
+  UPDATE sp_tmtk_banner SET sStatusDelete = 'V', sDeleteBy = p_sUserInput, dDeleteOn = CURRENT_TIMESTAMP  
+WHERE nUserId_fk = p_nUserId_fk  ;
+END;
+END IF;
+END */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `sp_tmtk_bracelet` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `sp_tmtk_bracelet` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_tmtk_bracelet`( p_sMode CHAR(1),	p_IdBracelet INT,
+	p_BraceletName CHAR(20),
+	p_BraceletDescription CHAR(100),
+	p_BraceletUploadFk CHAR(200)
+)
+BEGIN
+IF p_sMode = 'I' THEN 
+BEGIN
+DECLARE oId INT;
+	SET oId = (SELECT CASE WHEN COUNT(1) = 0 THEN 1 ELSE MAX(IdBracelet) + 1 END FROM tmtk_bracelet);
+ INSERT INTO tmtk_bracelet ( 			IdBracelet,
+			BraceletName,
+			BraceletDescription,
+			UploadFk,
+			dCreateOn ) 
+			VALUES ( 			oId,
+			p_BraceletName,
+			p_BraceletDescription,
+			p_BraceletUploadFk,
+			CURRENT_TIMESTAMP); 
+END;
+ELSEIF p_sMode = 'U' THEN 
+BEGIN
+ UPDATE sp_tmtk_banner SET 
+nUnitId_fk = p_nUnitId_fk,
+nSeqNo = p_nSeqNo,
+sUUID = MD5(UUID()),
+sLastEditBy = p_sUserInput, dLastEditOn = CURRENT_TIMESTAMP 
+WHERE nUserId_fk = p_nUserId_fk  ;
+END;
+ELSEIF p_sMode = 'D' THEN 
+BEGIN
+  UPDATE sp_tmtk_banner SET sStatusDelete = 'V', sDeleteBy = p_sUserInput, dDeleteOn = CURRENT_TIMESTAMP  
+WHERE nUserId_fk = p_nUserId_fk  ;
+END;
+END IF;
+END */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `sp_tmtk_Earrings` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `sp_tmtk_Earrings` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_tmtk_Earrings`( p_sMode CHAR(1),	p_IdEarrings INT,
+	p_EarringsName CHAR(20),
+	p_EarringsDescription CHAR(100),
+	p_EarringsUploadFk CHAR(200)
+)
+BEGIN
+IF p_sMode = 'I' THEN 
+BEGIN
+DECLARE oId INT;
+	SET oId = (SELECT CASE WHEN COUNT(1) = 0 THEN 1 ELSE MAX(IdEarrings) + 1 END FROM tmtk_Earrings);
+ INSERT INTO tmtk_Earrings ( 			IdEarrings,
+			EarringsName,
+			EarringsDescription,
+			UploadFk,
+			dCreateOn ) 
+			VALUES ( 			oId,
+			p_EarringsName,
+			p_EarringsDescription,
+			p_EarringsUploadFk,
+			CURRENT_TIMESTAMP); 
+END;
+ELSEIF p_sMode = 'U' THEN 
+BEGIN
+ UPDATE sp_tmtk_banner SET 
+nUnitId_fk = p_nUnitId_fk,
+nSeqNo = p_nSeqNo,
+sUUID = MD5(UUID()),
+sLastEditBy = p_sUserInput, dLastEditOn = CURRENT_TIMESTAMP 
+WHERE nUserId_fk = p_nUserId_fk  ;
+END;
+ELSEIF p_sMode = 'D' THEN 
+BEGIN
+  UPDATE sp_tmtk_banner SET sStatusDelete = 'V', sDeleteBy = p_sUserInput, dDeleteOn = CURRENT_TIMESTAMP  
+WHERE nUserId_fk = p_nUserId_fk  ;
+END;
+END IF;
+END */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `sp_tmtk_necklaces` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `sp_tmtk_necklaces` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_tmtk_necklaces`( p_sMode CHAR(1),	p_Idnecklencess INT,
+	p_necklencessName CHAR(20),
+	p_necklencessDescription CHAR(100),
+	p_necklencessUploadFk CHAR(200)
+)
+BEGIN
+IF p_sMode = 'I' THEN 
+BEGIN
+DECLARE oId INT;
+	SET oId = (SELECT CASE WHEN COUNT(1) = 0 THEN 1 ELSE MAX(Idnecklencess) + 1 END FROM tmtk_necklaces);
+ INSERT INTO tmtk_necklaces ( 			Idnecklencess,
+			necklencessName,
+			necklencessDescription,
+			UploadFk,
+			dCreateOn ) 
+			VALUES ( 			oId,
+			p_necklencessName,
+			p_necklencessDescription,
+			p_necklencessUploadFk,
+			CURRENT_TIMESTAMP); 
+END;
+ELSEIF p_sMode = 'U' THEN 
+BEGIN
+ UPDATE sp_tmtk_banner SET 
+nUnitId_fk = p_nUnitId_fk,
+nSeqNo = p_nSeqNo,
+sUUID = MD5(UUID()),
+sLastEditBy = p_sUserInput, dLastEditOn = CURRENT_TIMESTAMP 
+WHERE nUserId_fk = p_nUserId_fk  ;
+END;
+ELSEIF p_sMode = 'D' THEN 
+BEGIN
+  UPDATE sp_tmtk_banner SET sStatusDelete = 'V', sDeleteBy = p_sUserInput, dDeleteOn = CURRENT_TIMESTAMP  
+WHERE nUserId_fk = p_nUserId_fk  ;
+END;
+END IF;
+END */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `sp_tmtk_product_uploads` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `sp_tmtk_product_uploads` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_tmtk_product_uploads`( p_sMode CHAR(1),	p_IdUpload CHAR(100),
+	p_ProductId INT,
+	p_ProductPath CHAR(100)
+)
+BEGIN
+IF p_sMode = 'I' THEN 
+BEGIN
+DECLARE oId INT;
+	SET oId = (SELECT CASE WHEN COUNT(1) = 0 THEN 1 ELSE MAX(ProductId) + 1 END FROM tmtk_product_uploads);
+ INSERT INTO tmtk_product_uploads ( 			IdUpload,
+			ProductId,
+			ProductPath,
+			dCreateOn ) 
+			VALUES ( 			p_IdUpload,
+			oId,
+			p_ProductPath,
+			CURRENT_TIMESTAMP); 
+END;
+ELSEIF p_sMode = 'U' THEN 
+BEGIN
+ UPDATE sp_tmtk_banner SET 
+nUnitId_fk = p_nUnitId_fk,
+nSeqNo = p_nSeqNo,
+sUUID = MD5(UUID()),
+sLastEditBy = p_sUserInput, dLastEditOn = CURRENT_TIMESTAMP 
+WHERE nUserId_fk = p_nUserId_fk  ;
+END;
+ELSEIF p_sMode = 'D' THEN 
+BEGIN
+  UPDATE sp_tmtk_banner SET sStatusDelete = 'V', sDeleteBy = p_sUserInput, dDeleteOn = CURRENT_TIMESTAMP  
+WHERE nUserId_fk = p_nUserId_fk  ;
+END;
+END IF;
+END */$$
+DELIMITER ;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
