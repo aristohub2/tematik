5bc5b12727d82ba5a7a7efff339cb482c775731a
Xerather <alvin.janitra@gmail.com>
2019-10-24 10:08:20 +0700

    model added

--- frontend/application/controllers/Catalogue.php
+++ frontend/application/controllers/Catalogue.php
@@ -5,6 +5,7 @@ class Catalogue extends CI_Controller {
 
 	public function __construct(){
 		parent::__construct();
+		$this->load->model('mf_index');
 	}
 
 	public function index()
@@ -13,6 +14,16 @@ class Catalogue extends CI_Controller {
 		$data['js'] = $this->load->view('include/js.php',NULL,TRUE);
 		$data['css'] = $this->load->view('include/css.php',NULL,TRUE);
 		$data['footer'] = $this->load->view('include/footer.php',NULL,TRUE);
+
+		$data['banner1'] = $this->mf_index->get_banner();
+		$data['banner2'] = $this->mf_index->get_banner();
+		$data['banner3'] = $this->mf_index->get_banner();
+		$data['banner4'] = $this->mf_index->get_banner();
+		
+		$data['pic_earring'] = $this->mf_index->get_banner();
+		$data['pic_bracelet'] = $this->mf_index->get_banner();
+		$data['pic_pendant'] = $this->mf_index->get_banner();
+		$data['pic_ring'] = $this->mf_index->get_banner();
 		
 		$this->load->view('catalogue.php',$data);
 	}
