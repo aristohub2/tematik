c253e7e5d36b777093fc148bd370d2def9c8528c
Xerather <alvin.janitra@gmail.com>
2019-12-11 23:11:55 +0700

    v_index

--- backend/application/views/backend/src/index.php
+++ backend/application/views/backend/src/index.php
@@ -9,68 +9,19 @@
     <title>Backend</title>
 
     <?php echo $css; ?>
-    <?php echo $js; ?>
-
-    <style type="text/css">
-      ::-webkit-file-upload-button {
-      background: blue;
-      color: white;
-      border-radius: 25px;
-    }
-
-
-    /* Style the tab */
-    .tab {
-      overflow: hidden;
-      border: 1px solid #ccc;
-      background-color: #f1f1f1;
-    }
-
-    /* Style the buttons that are used to open the tab content */
-    .tab button {
-      background-color: inherit;
-      float: left;
-      border: none;
-      outline: none;
-      cursor: pointer;
-      padding: 14px 16px;
-      transition: 0.3s;
-    }
-
-    /* Change background color of buttons on hover */
-    .tab button:hover {
-      background-color: #ddd;
-    }
-
-    /* Create an active/current tablink class */
-    .tab button.active {
-      background-color: #ccc;
-    }
-
-    /* Style the tab content */
-    .tabcontent {
-      display: none;
-      padding: 6px 12px;
-      border: 1px solid #ccc;
-      border-top: none;
-    }
-
-    </style>    
+    <?php echo $js; ?>   
 </head>
 
 <body>
 
 <?php $this->load->view("backend/src/include/Sidebar.php") ?>
 
-    
-        <!-- tiap page berbeda mainnya disini aja -->
-        <!-- page-content  -->
         <main class="page-content pt-2">
             <div id="overlay" class="overlay"></div>
             <div class="container-fluid p-5">
                 <div class="row">
                     <div class="form-group col-md-12">
-                        <h2>Welcome <?php echo $_SESSION['user']; ?></h2>
+                        <h2>Welcome to Passion Prive - <?php echo $_SESSION['user']; ?></h2>
                     </div>
                 </div>
                 <hr>
@@ -78,32 +29,29 @@
         <!-- page-content" -->
     </div>
     <!-- page-wrapper -->
-
-    <?php echo $js?>
-
 </body>
 
 
 <script type="text/javascript">
     function openCity(evt, cityName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
+    // Declare all variables
+    var i, tabcontent, tablinks;
 
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
 
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
 </script>
 </html>
