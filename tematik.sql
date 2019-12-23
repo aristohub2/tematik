942b44c199b5ddba11d7924b933ffde3b058c2bc
Xerather <alvin.janitra@gmail.com>
2019-12-23 11:25:57 +0700

    FINAL PORTOFOLIO BUILD

--- tematik.sql
+++ tematik.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 15, 2019 at 05:43 PM
+-- Generation Time: Dec 23, 2019 at 05:25 AM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
